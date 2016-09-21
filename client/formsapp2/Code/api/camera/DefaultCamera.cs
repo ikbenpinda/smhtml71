cbfa837e3453136f597656702b7414148b380a37
ikbenpinda <etiennecooijmans@gmail.com>
2016-09-21 14:17:16 +0200

    Automatic storage of media.

--- client/formsapp2/Code/api/camera/DefaultCamera.cs
+++ client/formsapp2/Code/api/camera/DefaultCamera.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using ModernHttpClient;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using Xamarin.Forms;
@@ -8,6 +13,8 @@ namespace formsapp2
 {
 	public class DefaultCamera : ICamera
 	{
+		public const string DELIMITER = "__";
+
 		IStorage storage;
 		Page page;
 
@@ -31,16 +38,17 @@ namespace formsapp2
 				return null;
 			}
 
+			long id = Time.getUnixTimestamp(); // hack
 			var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
 			{
-				Directory = "photos",
-				Name = "test.jpg"
+				Name = "img" + id +".jpg",
+				SaveToAlbum = false
 			});
 
 			if (file == null)
 				return null;
 
-			page.DisplayAlert("File Location", file.Path, "OK");
+			page.DisplayAlert("foto opgeslagen: ", file.Path, "OK");
 
 			image.Source = ImageSource.FromStream(() =>
 			  {
@@ -48,6 +56,8 @@ namespace formsapp2
 				  file.Dispose();
 				  return stream;
 			  });
+
+			UploadPicture(file);
 
 			return image;
 		}
@@ -60,24 +70,79 @@ namespace formsapp2
 
 			if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakeVideoSupported)
 			{
-				page.DisplayAlert("No Camera", ":( No camera available.", "OK");
+				//page.DisplayAlert("No Camera", ":( No camera available.", "OK");
 				return null;
 			}
 
+			var id = Time.getUnixTimestamp();
 			var file = await CrossMedia.Current.TakeVideoAsync(
 				new StoreVideoOptions{
-					Directory = "videos",
-					Name = "vid.mp4" // Automatically numbered/suffixed with "_[id]".
+					Name = "vid" + id + ".mp4", // Automatically numbered/suffixed with "_[id]".
+					SaveToAlbum = false,
 			});
 
 			if (file == null)
 				return null;
 
-			page.DisplayAlert("File Location", file.Path, "OK");
+			page.DisplayAlert("video opgeslagen: ", file.Path, "OK");
 
 			file.Dispose();
 
 			return file;
+		}
+
+		string baseurl = "http://145.93.114.157:8081";
+		IUserRepository user_db = new FakeUserRepository();
+
+		public void UploadPicture(MediaFile file)
+		{
+			var user = user_db.load();
+
+			var path = file.Path;
+			Debug.WriteLine("PATH: " + path);
+
+			int index = file.Path.LastIndexOf(@"/");
+			Debug.WriteLine("INDEX: " + index);
+
+			var fn = file.Path.Substring(index + 1);
+			Debug.WriteLine("FILENAME: " + fn);
+
+			if (file == null)
+				return;
+			
+			try
+			{
+				HttpClient client = new HttpClient(new NativeMessageHandler());
+				client.BaseAddress = new Uri(baseurl);
+				byte[] data = fileToByteArray(file);
+				var fileContent = new ByteArrayContent(data);
+				fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+				{
+					Name = "file",
+					FileName = user.name + DELIMITER + fn
+				};
+				Debug.WriteLine("UPLOADING: " + fileContent.Headers.ContentDisposition.FileName);
+				fileContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+				var content = new MultipartFormDataContent();
+				content.Add(fileContent);
+
+				var result = client.PostAsync("/fileUpload", content).Result;
+				Debug.WriteLine(result);
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+		}
+
+		public byte[] fileToByteArray(MediaFile file)
+		{
+			using (var memoryStream = new MemoryStream())
+			{
+				file.GetStream().CopyTo(memoryStream);
+				file.Dispose();
+				return memoryStream.ToArray();
+			}
 		}
 	}
 }
--- client/formsapp2/Code/api/storage/FakeMediaStorage.cs
+++ client/formsapp2/Code/api/storage/FakeMediaStorage.cs
@@ -1,21 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using PCLStorage;
+using Xamarin.Forms;
+
 namespace formsapp2
 {
 	public class FakeMediaStorage : IStorage
 	{
 		public FakeMediaStorage() { 
-			System.Diagnostics.Debug.WriteLine("Warning: Using fake MediaStorage.");
+			Debug.WriteLine("Warning: Using fake MediaStorage.");
 		}
 
 		public Media get(int id)
 		{
-			System.Diagnostics.Debug.WriteLine("Loading image has not been implemented yet.");
+			Debug.WriteLine("Loading image has not been implemented yet.");
 			return null;
+		}
+
+		public async Task<List<Image>> getAllPictures()
+		{
+			Debug.WriteLine("LocalStorage path: " + FileSystem.Current.LocalStorage.Path);
+
+			var pictures = "";
+			var android = true;
+			Device.OnPlatform(
+				iOS: () => { pictures = "../Documents"; android = false; },
+				Android: () => { pictures = "Pictures"; android = true; }
+			);
+
+			IFolder folder = null;
+
+			if (android)
+			{ 	// hack - PCLStorage and MediaPlugin don't play well together.
+			  	// hack - Don't use Device.OnPlatform because of threading issues.
+				// todo just get it from the server, fuck this.
+				//await FileSystem.Current.LocalStorage.CreateFolderAsync("Pictures", CreationCollisionOption.OpenIfExists);
+				Debug.WriteLine("STORAGEPATH: Searching...");
+
+				try
+				{
+					folder = FileSystem.Current.GetFolderFromPathAsync("/storage/emulated/0/Android/data/com.companyname.formsapp2/files/Pictures").Result;		
+					if (folder == null)
+					{
+						throw new Exception("folder is null.");
+					}
+				}
+				catch (Exception ex)
+				{
+					Debug.WriteLine("EX: " + ex.Message);
+					return new List<Image>();
+				}
+				//folder = await FileSystem.Current.LocalStorage.GetFolderAsync("Pictures");
+			}
+			else {
+				Debug.WriteLine("STORAGEPATH: Searching...");
+				folder = await FileSystem.Current.LocalStorage.GetFolderAsync(pictures);
+			}
+			
+			//var file = await android_folder.GetFileAsync(filename);
+			Debug.WriteLine("STORAGEPATH: " + folder);
+			List<Image> images = new List<Image>();
+			foreach (IFile file in await folder.GetFilesAsync())
+			{
+				Image image = new Image() { 
+					Aspect = Aspect.AspectFit, 
+					Source = file.Path
+				};
+				Debug.WriteLine("FILES: Added file: " + file.Path);
+				images.Add(image);
+			}
+
+			return images;
 		}
 
 		public void save(Media media)
 		{
-			System.Diagnostics.Debug.WriteLine("Saving media(" + media.title + ")...");
+			Debug.WriteLine("Saving media(" + media.title + ")...");
 		}
 	}
 }
